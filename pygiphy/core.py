e0e4039b0107aa9469cc969e7f75bf50214445fd
Zach Schumacher <zachary@simplebet.io>
2022-05-16 19:45:08 -0400

    pragma

--- pygiphy/core.py
+++ pygiphy/core.py
@@ -5,7 +5,7 @@ from typing import TypeVar
 
 import httpx
 
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 8):  # pragma: no cover
     from typing_extensions import Final
 else:
     from typing import Final
